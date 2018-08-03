a60ef2609d44ae204e6193320a031ff32846427d
Arius <arius@filtered.ai>
2018-08-03 12:45:57 -0400

    Initial commit of project contents

---
+++ Assets/PlayerKiller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerKiller : MonoBehaviour {
+	public GameObject PlayerStartPosition;
+
+	// Use this for initialization
+	void Start () {
+		// set the transform.position equal to that of the start position
+	}
+
+	private void OnTriggerEnter2D (Collider2D other){
+
+		if (other.gameObject.name.StartsWith ("PK")) {
+			transform.rotation = PlayerStartPosition.transform.rotation;
+			transform.position = PlayerStartPosition.transform.position;
+
+		}
+	}
+}
+	
---
+++ Assets/playerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+
+	// Use this for initialization
+
+	
+	// Update is called once per frame
+	void Update() {
+
+		if (Input.GetKey (KeyCode.RightArrow))
+			transform.Translate (.125f, 0, 0);
+		if (Input.GetKey (KeyCode.LeftArrow))
+			transform.Translate (-.125f, 0, 0);
+
+
+	}
+}
