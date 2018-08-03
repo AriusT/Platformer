7c4f5b1538aa10bbafe1a4fa690159bc2e77d015
Arius <arius@filtered.ai>
2018-08-03 14:30:59 -0400

    Tried to fix jumping

--- Assets/Jump.cs
+++ Assets/Jump.cs
@@ -8,7 +8,7 @@ public class Jump : MonoBehaviour {
 	public float jumpVelocity;
 
 	void Update () {
-		if (Input.GetButtonDown ("Jump")) {
+		if (Input.GetButtonUp ("Jump")) {
 			GetComponent<Rigidbody2D> ().velocity = Vector2.up * jumpVelocity;
 		}
 	}
